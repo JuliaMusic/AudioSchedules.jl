027b7dc737a581529a8c59a09b2514c974f7d265
Brandon Taylor <brandon.taylor221@gmail.com>
2020-05-27 14:45:55 -0400

    fix

--- test/justly.jl
+++ test/justly.jl
@@ -2,8 +2,8 @@ using PortAudio: PortAudioStream
 using Unitful: Hz, s
 import JSON
 
-function make_envelope(duration)
-    Envelope((0, 0.1, 0), (duration / 2, duration / 2), (Line, Line))
+function make_envelope(duration, ramp = 0.05s)
+    Envelope((0, 0.1, 0.1, 0), (ramp, duration - ramp - ramp, ramp), (Line, Line, Line))
 end
 
 function make_interval(note)
@@ -25,13 +25,13 @@ function justly!(schedule, song, key, seconds_per_beat)
                 make_envelope(note["beats"] * seconds_per_beat),
             )
         end
-        clock = clock + modulation["beats"]s
+        clock = clock + modulation["beats"] * seconds_per_beat
     end
 end
 
 const SAMPLE_RATE = 44100Hz
 a_schedule = AudioSchedule(SAMPLE_RATE)
-justly!(a_schedule, read("all_i_have_to_do_is_dream.json", String), 440Hz, 1s)
+justly!(a_schedule, read("all_i_have_to_do_is_dream.json", String), 440Hz, 2s)
 a_plan = Plan(a_schedule)
 stream = PortAudioStream(samplerate = SAMPLE_RATE / Hz)
 write(stream.sink, a_plan, length(a_plan))
