2abdcef38d408b93689c4718eb0202503d828894
Brandon Taylor <brandon.taylor221@gmail.com>
2020-05-27 14:36:37 -0400

    fix

--- src/AudioSchedules.jl
+++ src/AudioSchedules.jl
@@ -8,20 +8,20 @@ import SampledSignals: samplerate, nchannels, unsafe_read!
 using SampledSignals: Hz, s, SampleSource
 const TAU = 2 * pi
 
-mutable struct InfiniteStateful{Iterator, Item, State}
+mutable struct StrictStateful{Iterator,Item,State}
     iterator::Iterator
-    item_state::Tuple{Item, State}
+    item_state::Tuple{Item,State}
 end
-IteratorSize(::Type{<:InfiniteStateful}) = IsInfinite()
-IteratorEltype(::Type{InfiniteStateful{<:Any, Item, <:Any}}) where {Item} = Item
-InfiniteStateful(iterator) = InfiniteStateful(iterator, iterate(iterator))
-function iterate(stateful::InfiniteStateful, state = nothing)
+IteratorSize(::Type{<:StrictStateful}) = IsInfinite()
+IteratorEltype(::Type{StrictStateful{<:Any,Item,<:Any}}) where {Item} = Item
+StrictStateful(iterator) = StrictStateful(iterator, iterate(iterator))
+function iterate(stateful::StrictStateful, state = nothing)
     last_item, state = stateful.item_state
     stateful.item_state = iterate(stateful.iterator, state)
     last_item, nothing
 end
 
-mutable struct Plan{InnerIterator} <: SampleSource where {InnerIterator<:Stateful}
+mutable struct Plan{InnerIterator} <: SampleSource
     outer_iterator::Vector{Tuple{InnerIterator,Int}}
     outer_state::Int
     inner_iterator::InnerIterator
@@ -62,7 +62,7 @@ end
 @noinline function inner_fill!(inner_iterator, item, buf, a_range)
     for index in a_range
         @inbounds buf[index] = item
-        item::Float64, inner_state = iterate(inner_iterator)
+        item, inner_state = iterate(inner_iterator)
     end
     item
 end
@@ -75,7 +75,7 @@ end
     buf,
     frameoffset,
     framecount,
-    outer_result::Tuple{Tuple{Any,Any},Any},
+    outer_result,
     until,
 )
     (inner_iterator, source.has_left), source.outer_state = outer_result
@@ -117,49 +117,36 @@ Return an iterator that will the play the `synthesizer` at `the_sample_rate`
 make_iterator(synthesizer, the_sample_rate) = synthesizer
 export make_iterator
 
-struct InfiniteMapIterator{AFunction,Iterators}
+struct StrictMapIterator{AFunction,Iterators}
     a_function::AFunction
     iterators::Iterators
 end
 
-IteratorSize(::Type{<:InfiniteMapIterator}) = IsInfinite
+IteratorSize(::Type{<:StrictMapIterator}) = IsInfinite
 
-IteratorEltype(::Type{<:InfiniteMapIterator}) = EltypeUnknown
+IteratorEltype(::Type{<:StrictMapIterator}) = EltypeUnknown
 
-@inline _my_first(something, rest...) = something
-@inline my_first(them) = _my_first(them...)
-
-@inline map_unrolled(a_function, them) =
-    a_function(my_first(them)), map_unrolled(a_function, tail(them))...
-@inline map_unrolled(a_function, ::Tuple{}) = ()
-@inline map_unrolled(a_function, tuple_1, tuple_2) =
-    a_function(my_first(tuple_1), my_first(tuple_2)), map_unrolled(a_function, tail(tuple_1), tail(tuple_2))...
-@inline map_unrolled(a_function, ::Tuple{}, ::Tuple{}) = ()
-
-@inline _my_last(first_one, second_one) = second_one
-@inline my_last(pair) = _my_last(pair...)
-
-@inline function iterate(something::InfiniteMapIterator, state...)
-    items_states = map_unrolled(iterate, something.iterators, state...)
-    something.a_function(map_unrolled(my_first, items_states)...), map_unrolled(my_last, items_states)
+@inline function iterate(something::StrictMapIterator, state...)
+    items_states = map(iterate, something.iterators, state...)
+    something.a_function(map(first, items_states)...), map(last, items_states)
 end
 
 """
-    InfiniteMap(a_function, synthesizers...)
+    StrictMap(a_function, synthesizers...)
 
 Map `a_function` over `synthesizers`, assuming that none of the `synthesizers` will end
-early.
+before they are scheduled to.
 """
-struct InfiniteMap{AFunction,Synthesizers} <: Synthesizer
+struct StrictMap{AFunction,Synthesizers} <: Synthesizer
     a_function::AFunction
     synthesizers::Synthesizers
-    InfiniteMap(a_function::AFunction, synthesizers...) where {AFunction} =
+    StrictMap(a_function::AFunction, synthesizers...) where {AFunction} =
         new{AFunction,typeof(synthesizers)}(a_function, synthesizers)
 end
-export InfiniteMap
+export StrictMap
 
-function make_iterator(a_map::InfiniteMap, the_sample_rate)
-    InfiniteMapIterator(
+function make_iterator(a_map::StrictMap, the_sample_rate)
+    StrictMapIterator(
         a_map.a_function,
         map(synthesizer -> make_iterator(synthesizer, the_sample_rate), a_map.synthesizers),
     )
@@ -278,39 +265,39 @@ struct Envelope{Levels,Durations,Shapes}
 end
 export Envelope
 
-const ORCHESTRA = Dict{Symbol,Synthesizer}
+const ORCHESTRA = Dict{Symbol,Tuple{StrictStateful,Bool}}
 const TRIGGERS = SortedDict{Float64,Vector{Tuple{Symbol,Bool}}}
 
 mutable struct AudioSchedule
     orchestra::ORCHESTRA
     triggers::TRIGGERS
+    the_sample_rate::Int
 end
 
 """
-    AudioSchedule()
+    AudioSchedule(the_sample_rate)
 
 Create an `AudioSchedule`.
 
 ```jldoctest schedule
 julia> using AudioSchedules
 
-julia> using Unitful: s, Hz
+julia> using SampledSignals: s, Hz
 
-julia> a_schedule = AudioSchedule()
+julia> a_schedule = AudioSchedule(44100Hz)
 AudioSchedule with triggers at () seconds
 ```
 
-Add a synthesizer to the schedule with [`schedule!`](@ref). You can schedule for a duration
-in seconds, or use an [`Envelope`](@ref).
+Add a synthesizer to the schedule with [`schedule!`](@ref).
 
 ```jldoctest schedule
 julia> envelope = Envelope((0, 0.25, 0), (0.05s, 0.95s), (Line, Line));
 
-julia> schedule!(a_schedule, InfiniteMap(sin, Cycles(440Hz)), 0s, envelope)
+julia> schedule!(a_schedule, StrictMap(sin, Cycles(440Hz)), 0s, envelope)
 
-julia> schedule!(a_schedule, InfiniteMap(sin, Cycles(440Hz)), 1s, envelope)
+julia> schedule!(a_schedule, StrictMap(sin, Cycles(440Hz)), 1s, envelope)
 
-julia> schedule!(a_schedule, InfiniteMap(sin, Cycles(550Hz)), 1s, envelope)
+julia> schedule!(a_schedule, StrictMap(sin, Cycles(550Hz)), 1s, envelope)
 
 julia> a_schedule
 AudioSchedule with triggers at (0.0, 0.05, 1.0, 1.05, 2.0) seconds
@@ -321,7 +308,7 @@ Then, you can create a `SampledSource` from the schedule using [`Plan`](@ref).
 ```jldoctest schedule
 julia> using SampledSignals: unsafe_read!
 
-julia> a_plan = Plan(a_schedule, 44100Hz);
+julia> a_plan = Plan(a_schedule);
 ```
 
 You can find the number of samples in a `Plan` with length.
@@ -338,31 +325,21 @@ julia> buf = Vector{Float64}(undef, the_length);
 
 julia> unsafe_read!(a_plan, buf, 0, the_length);
 
-julia> buf[1:4] == [0.0, 7.102984600764591e-6, 2.835612782188846e-5, 6.359232681199096e-5]
+julia> buf[1:4] == [0.0, 1.417806391094423e-5, 4.23948845413273e-5, 8.44006285522918e-5]
 true
 ```
 """
-AudioSchedule() = AudioSchedule(ORCHESTRA(), TRIGGERS())
+AudioSchedule(the_sample_rate) =
+    AudioSchedule(ORCHESTRA(), TRIGGERS(), the_sample_rate / Hz)
 export AudioSchedule
 
-"""
-    schedule!(schedule::AudioSchedule, synthesizer::Synthesizer, start_time, duration)
-
-Schedule an audio synthesizer to be added to the `schedule`, starting at `start_time` and
-lasting for `duration`. You can also pass an [`Envelope`](@ref) as a duration. See the
-example for [`AudioSchedule`](@ref).
-"""
-function schedule!(
-    a_schedule::AudioSchedule,
-    synthesizer::Synthesizer,
-    start_time,
-    duration,
-)
+function schedule_segment!(a_schedule::AudioSchedule, iterator, start_time, duration)
     start_time_unitless = start_time / s
     triggers = a_schedule.triggers
     label = gensym("instrument")
     stop_time = start_time_unitless + duration / s
-    a_schedule.orchestra[label] = synthesizer
+    a_schedule.orchestra[label] =
+        StrictStateful(iterator), false
     start_trigger = label, true
     if haskey(triggers, start_time_unitless)
         push!(triggers[start_time_unitless], start_trigger)
@@ -378,24 +355,33 @@ function schedule!(
     nothing
 end
 
-function schedule!(
-    a_schedule::AudioSchedule,
-    synthesizer::Synthesizer,
-    start_time,
-    envelope::Envelope,
-)
+"""
+    schedule!(schedule::AudioSchedule, synthesizer::Synthesizer, start_time, envelope::Envelope)
+
+Schedule an audio synthesizer to be added to the `schedule`, starting at `start_time` with
+the duration and volume contained in an [`Envelope`](@ref). See the example for
+[`AudioSchedule`](@ref).
+"""
+function schedule!(a_schedule::AudioSchedule, synthesizer, start_time, envelope::Envelope)
+    the_sample_rate = a_schedule.the_sample_rate
     durations = envelope.durations
     levels = envelope.levels
     shapes = envelope.shapes
     index = 1
+    stateful_iterator = StrictStateful(make_iterator(synthesizer, the_sample_rate))
     for index = 1:length(durations)
         duration = durations[index]
-        schedule!(
+        schedule_segment!(
             a_schedule,
-            InfiniteMap(
+            StrictMapIterator(
                 *,
-                synthesizer,
-                shapes[index](levels[index], levels[index+1], duration),
+                (
+                    stateful_iterator,
+                    make_iterator(
+                        shapes[index](levels[index], levels[index+1], duration),
+                        the_sample_rate,
+                    ),
+                ),
             ),
             start_time,
             duration,
@@ -413,7 +399,7 @@ function conduct(::Tuple{})
 end
 
 function conduct(iterators)
-    InfiniteMapIterator(+, iterators)
+    StrictMapIterator(+, iterators)
 end
 
 """
@@ -421,28 +407,26 @@ end
 
 Return a `SampledSource` for the schedule.
 """
-function Plan(a_schedule::AudioSchedule, the_sample_rate)
-    the_sample_rate_unitless = the_sample_rate / Hz
+function Plan(a_schedule::AudioSchedule)
+    the_sample_rate = a_schedule.the_sample_rate
+    orchestra = a_schedule.orchestra
     time = Ref(0.0)
-    stateful_orchestra = Dict(
-        (label, (InfiniteStateful(make_iterator(synthesizer, the_sample_rate_unitless)), false)) for (label, synthesizer) in pairs(a_schedule.orchestra)
-    )
     Plan(
         [
             begin
                 together = conduct(((
-                    iterator for (iterator, is_on) in values(stateful_orchestra) if is_on
+                    iterator for (iterator, is_on) in values(orchestra) if is_on
                 )...,))
                 for (label, is_on) in trigger_list
-                    iterator, _ = stateful_orchestra[label]
-                    stateful_orchestra[label] = iterator, is_on
+                    iterator, _ = orchestra[label]
+                    orchestra[label] = iterator, is_on
                 end
-                samples = round(Int, (end_time - time[]) * the_sample_rate_unitless)
+                samples = round(Int, (end_time - time[]) * the_sample_rate)
                 time[] = end_time
                 together, samples
             end for (end_time, trigger_list) in pairs(a_schedule.triggers)
         ],
-        the_sample_rate_unitless,
+        the_sample_rate,
     )
 end
 export Plan
