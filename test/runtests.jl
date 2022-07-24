5ea870d655377135a0edcbdaf374154ee67160e3
Brandon Taylor <brandon.taylor221@gmail.com>
2022-07-23 21:59:39 -0400

    extra test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,23 @@
-using AudioSchedules: AudioSchedules
+using AudioSchedules
+using Test: @test
 using Documenter: doctest
 
 doctest(AudioSchedules)
+
+# test gaps in audio scheduels
+a_schedule = AudioSchedule()
+push!(a_schedule, Map(sin, Cycles(440Hz)), 0s, @envelope(
+    0,
+    Line => 1s,
+    1,
+    Line => 1s,
+    0
+))
+push!(a_schedule, Map(sin, Cycles(440Hz)), 4s, @envelope(
+    0,
+    Line => 1s,
+    1,
+    Line => 1s,
+    0
+))
+@test first(collect(a_schedule)[3]) ≈ 0
